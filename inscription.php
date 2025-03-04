f08d41bfa3743dcd8ec1ba0b14e32046350be216
ridah00 <mahdjoubridah00@gmail.com>
2025-03-04 10:08:12 +0100

    petite retouche

--- inscription.php
+++ inscription.php
@@ -4,7 +4,7 @@ $regex_pseudo = "/^[a-zA-Z0-9-_.]{5,20}$/";
 $regex_email = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
 $regex_pwd_8 = "/[a-zA-Z0-9_@.]{8,30}$/";
 $date = date('Y-m-d');
-
+$c_password_value='';
 $min_date = date('Y-m-d', strtotime("-18 years"));
 
 $errors = [];
@@ -46,6 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
         $errors['password'] = 'au moins 8 caracteres';
     }
 
+    (!array_key_exists('c_password',$errors)) ?  $c_password_value = $_POST['c_password'] : '';
     if (empty($_POST["c_password"])) {
         $errors['c_password'] = 'champs obligatoire';
     } elseif ($_POST["c_password"] != $_POST["password"]) {
@@ -54,10 +55,10 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
 
     if (empty($_POST["birthdate"])) {
         $errors['birthdate'] = 'champ obligatoire';
-    }else if ($_POST['birthdate'] > $min_date) {
+    } else if ($_POST['birthdate'] > $min_date) {
         $errors['birthdate'] = 'age minimum autorisé : 18 ans';
     }
-    
+
     if (empty($_POST["gender"])) {
         $errors['gender'] = 'champs obligatoire';
     } elseif ($_POST["gender"] != 'homme' && $_POST["gender"] != 'femme') {
@@ -66,15 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     if (!isset($_POST['condition'])) {
         $errors['condition'] = 'champs obligatoire';
     }
-    
-    
 }
-
-
-
-
-
-
 
 
 
@@ -107,55 +100,66 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
 </style>
 
 <body class="anim">
-    <div class="container mt-5 bg-secondary">
+    <div class="container mt-5 bg-secondary-subtle">
         <form method="POST" novalidate>
             <div class="row m-0 gap-3">
                 <div class="col-sm p-0">
-                    <label for="nom" class="form-label">Nom</label><span class="px-2 text-danger"><?= $errors['lastname'] ?? '' ?></span>
+                    <label for="nom" class="form-label">Nom</label>
                     <input type="text" class="form-control" name="lastname" id="nom" placeholder="Entrez votre nom" value="<?= $_POST["lastname"] ?? '' ?>" required>
+                    <span class="px-2 text-danger"><?= $errors['lastname'] ?? '' ?></span>
                 </div>
                 <div class="col-sm p-0">
-                    <label for="prenom" class="form-label">Prénom</label><span class="px-2 text-danger"><?= $errors['firstname'] ?? '' ?></span>
+                    <label for="prenom" class="form-label">Prénom</label>
                     <input type="text" class="form-control" name="firstname" id="prenom" placeholder="Entrez votre prénom" value="<?= $_POST["firstname"] ?? '' ?>" required>
+                    <span class="px-2 text-danger"><?= $errors['firstname'] ?? '' ?></span>
                 </div>
             </div>
             <div class="col-sm p-0">
-                    <label for="pseudo" class="form-label">Pseudo</label><span class="px-2 text-danger"><?= $errors['pseudo'] ?? '' ?></span>
-                    <input type="text" class="form-control" name="pseudo" id="pseudo" placeholder="Entrez votre pseudo" value="<?= $_POST["pseudo"] ?? '' ?>" required>
-                </div>
+                <label for="pseudo" class="form-label">Pseudo</label>
+                <input type="text" class="form-control" name="pseudo" id="pseudo" placeholder="Entrez votre pseudo" value="<?= $_POST["pseudo"] ?? '' ?>" required>
+                <span class="px-2 text-danger"><?= $errors['pseudo'] ?? '' ?></span>
+            </div>
             <div>
-                <label for="email" class="form-label">Email</label><span class="px-2 text-danger"><?= $errors['mail'] ?? '' ?></span>
+                <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" name="mail" id="email" placeholder="Entrez votre email" value="<?= $_POST["mail"] ?? '' ?>" required>
+                <span class="px-2 text-danger"><?= $errors['mail'] ?? '' ?></span>
             </div>
             <div class="row m-0 gap-3">
                 <div class="col-sm p-0">
-                    <label for="mdp" class="form-label">Mot de passe</label><span class="px-2 text-danger"><?= $errors['password'] ?? '' ?></span>
+                    <label for="mdp" class="form-label">Mot de passe</label>
                     <input type="password" class="form-control" name="password" id="mdp" placeholder="Entrez votre mot de passe" value="<?= $_POST["password"] ?? '' ?>"
                         required>
+                    <span class="px-2 text-danger"><?= $errors['password'] ?? '' ?></span>
                 </div>
                 <div class="col-sm p-0">
-                    <label for="confirmationmdp" class="form-label">Confirmation du mot de passe</label><span class="px-2 text-danger"><?= $errors['c_password'] ?? '' ?></span>
-                    <input type="password" class="form-control" name="c_password" id="confirmationmdp" value="<?= $_POST["c_password"] ?? '' ?>"
+                    <label for="confirmationmdp" class="form-label">Confirmation du mot de passe</label>
+                    <input type="password" class="form-control" name="c_password" id="confirmationmdp" value="<?= $c_password_value ?>"
                         placeholder="Confirmez votre mot de passe" required>
+                    <span class="px-2 text-danger"><?= $errors['c_password'] ?? '' ?></span>
                 </div>
             </div>
             <div class="row m-0 gap-3">
                 <div class="col-sm p-0">
-                    <label for="date_naissance" class="form-label">Date de naissance</label><span class="px-2 text-danger"><?= $errors['birthdate'] ?? '' ?></span>
+                    <label for="date_naissance" class="form-label">Date de naissance</label>
                     <input type="date" max="<?= $date ?>" class="form-control" name="birthdate" id="date_naissance" value="<?= $_POST["birthdate"] ?? '' ?>" required>
+                    <span class="px-2 text-danger"><?= $errors['birthdate'] ?? '' ?></span>
                 </div>
                 <div class="col-sm p-0">
-                    <label class="form-label">Genre</label><span class="px-2 text-danger"><?= $errors['gender'] ?? '' ?></span>
+                    <label class="form-label">Genre</label>
                     <select class="form-select" name="gender" required>
                         <option value="">Selectionnez</option>
                         <option <?= (isset(($_POST['gender'])) && ($_POST['gender']) == 'homme') ? 'selected' : '' ?> value="homme">Homme</option>
                         <option <?= (isset(($_POST['gender'])) && ($_POST['gender']) == 'femme') ? 'selected' : '' ?> value="femme">Femme</option>
                     </select>
+                    <span class="px-2 text-danger"><?= $errors['gender'] ?? '' ?></span>
                 </div>
             </div>
-            <div class="form-check d-flex justify-content-center align-items-center m-3">
-                <input type="checkbox" name="condition" class="form-check-input" id="conditions" <?= isset(($_POST['condition'])) ? 'checked' : '' ?> required>
-                <label class="form-check-label mx-2" for="conditions">J'accepte les conditions d'utilisation</label><span class="px-2 text-danger"><?= $errors['condition'] ?? '' ?></span>
+            <div class="">
+                <div class="form-check align-items-center m-3 d-flex justify-content-center">
+                    <input type="checkbox" name="condition" class="form-check-input" id="conditions" <?= isset(($_POST['condition'])) ? 'checked' : '' ?> required>
+                    <label class="form-check-label mx-2" for="conditions">J'accepte les conditions d'utilisation</label>
+                    <span class="px-2 text-danger d-block"><?= $errors['condition'] ?? '' ?></span>
+                </div>
             </div>
             <div class="d-flex justify-content-center">
                 <button type="submit" class="btn btn-primary ">Inscrire</button>
